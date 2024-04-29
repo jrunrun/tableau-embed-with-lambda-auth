9d28be09744f49066e673b6b307f4adf393a9c57
jrunrun <jpcraycraft@gmail.com>
2024-04-29 12:20:32 -0700

    local exe of python jwt

---
+++ lambda/local-jwt.py
@@ -0,0 +1,31 @@
+import json, jwt, uuid, os, datetime
+
+CLIENT_ID = os.environ["CLIENT_ID"]
+SECRET_ID = os.environ["SECRET_ID"]
+SECRET = os.environ["SECRET"]
+
+def lambda_handler(username):
+
+    token = jwt.encode(
+        {
+            "iss": CLIENT_ID,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
+            "jti": str(uuid.uuid4()),
+            "aud": "tableau",
+            "sub": username,
+            "scp": ["tableau:views:embed", "tableau:insights:embed"]
+        },
+        SECRET,
+        algorithm="HS256",
+        headers={
+            'kid': SECRET_ID,
+            'iss': CLIENT_ID
+        }
+    )
+
+    print(token)
+    
+    return token
+
+lambda_handler('embedusertest24@gmail.com')
+
